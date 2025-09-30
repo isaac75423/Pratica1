eefaaca74971575d478be561b95c94a78d88d73d
IsaacFCM <102389040+IsaacFCM@users.noreply.github.com>
2025-09-29 22:03:23 -0600

    Avance -funcionamiento de boton con Controller

--- view/Inicio/IniciarSesion.php
+++ view/Inicio/IniciarSesion.php
@@ -1,3 +1,7 @@
+<?php
+  include_once $_SERVER['DOCUMENT_ROOT'] .'/Curso/Pratica1/Pratica1/Controller/InicioController.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -33,25 +37,31 @@
                             <a href="index.html" class="">
                                 <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>DarkPan</h3>
                             </a>
-                            <h3>Sign In</h3>
+                            <h3>Iniciar Sesion</h3>
                         </div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
+
+                        <form id="formAuthentication" class="mb-3" action="" method="POST"> 
+
+                            <div class=" mb-3">
+                            <input type="email" class="form-control" id="CorreoElectronico" name="CorreoElectronico" placeholder="ejemplo@gmail.com">
+                            <label for="floatingInput">Correo Electronico</label>
                         </div>
-                        <div class="form-floating mb-4">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Password</label>
+                        <div class=" mb-4">
+                            <input type="password" class="form-control" id="Contraseña" name="Contraseña" placeholder="Contraseña">
+                            <label for="floatingPassword">Contraseña</label>
                         </div>
                         <div class="d-flex align-items-center justify-content-between mb-4">
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                <label class="form-check-label" for="exampleCheck1">Mostrar contraseña</label>
                             </div>
-                            <a href="">Forgot Password</a>
+                            <a href="">Recuperar contraseña</a>
                         </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                        <p class="text-center mb-0">Don't have an Account? <a href="">Sign Up</a></p>
+                        <button class="btn btn-primary py-3 w-100 mb-4" id="btnIniciarSesion" name="btnIniciarSesion" type="subit">Iniciar Sesion</button>
+
+                        <p class="text-center mb-0">No tienes una cuenta? <a href="Registro.php">Crear cuenta</a></p>
+
+                        </form> 
                     </div>
                 </div>
             </div>
--- view/Inicio/Registro.php
+++ view/Inicio/Registro.php
@@ -33,29 +33,29 @@
                             <a href="index.html" class="">
                                 <h3 class="text-primary"><i class="fa fa-user-edit me-2"></i>DarkPan</h3>
                             </a>
-                            <h3>Sign Up</h3>
+                            <h3>Registro</h3>
                         </div>
                         <div class="form-floating mb-3">
                             <input type="text" class="form-control" id="floatingText" placeholder="jhondoe">
-                            <label for="floatingText">Username</label>
+                            <label for="floatingText">Nombre de usuario</label>
                         </div>
                         <div class="form-floating mb-3">
                             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
+                            <label for="floatingInput">Correo electrónico</label>
                         </div>
                         <div class="form-floating mb-4">
                             <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Password</label>
+                            <label for="floatingPassword">Contraseña</label>
                         </div>
                         <div class="d-flex align-items-center justify-content-between mb-4">
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                <label class="form-check-label" for="exampleCheck1">Mostrar contraseña</label>
                             </div>
-                            <a href="">Forgot Password</a>
+                           
                         </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign Up</button>
-                        <p class="text-center mb-0">Already have an Account? <a href="">Sign In</a></p>
+                        <a href="IniciarSesion.php" class="btn btn-primary py-3 w-100 mb-4">Crear cuenta</a>
+                        <p class="text-center mb-0">¿Ya tienes una cuenta? <a href="">Ingresar</a></p>
                     </div>
                 </div>
             </div>
