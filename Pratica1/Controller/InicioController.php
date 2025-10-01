2f441a55df7a0a6fabd530d74163e334a0d0fef8
isaac75423 <164393217+isaac75423@users.noreply.github.com>
2025-09-30 19:21:46 -0600

    Delete Pratica1 directory

--- Pratica1/Controller/InicioController.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    if(isset($_POST["btnIniciarSesion"]))
-    {
-        header("Location: ../../view/Inicio/Principal.php");
-    }
-
-?>
--- Pratica1/README.md
+++
@@ -1,2 +0,0 @@
-# Pratica1
-pratica 1 web/cliente servidor 2025
